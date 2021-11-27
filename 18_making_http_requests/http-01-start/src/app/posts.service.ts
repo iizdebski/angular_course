ee83cad5e2929e455ff21e4c106e1fa02ecd8f98
SwAppCoder <iizdepskyy112@gmail.com>
2021-11-27 13:35:57 +0200

    angular_course

---
+++ 18_making_http_requests/http-01-start/src/app/posts.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHandler, HttpHeaders, HttpParams, HttpEventType } from '@angular/common/http';
+import { map, catchError, tap} from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
+
+import { Post } from './post.model';
+
+@Injectable({providedIn: 'root'})
+export class PostsService {
+  error = new Subject<string>();
+
+    constructor(private http: HttpClient) {}
+
+
+    createAndStorePost(title: string, content: string) {
+        const postData: Post = {title: title, content: content};
+        this.http
+        .post<{name: string}>(
+          'https://angular-httprequests-project.firebaseio.com/', 
+          postData,
+          {
+            observe: 'response'
+          }
+          )
+          .subscribe(
+            responseData => {
+            console.log(responseData);
+          }, error => {
+            this.error.next(error.message);
+          });
+    }
+
+    fetchPosts() {
+      let searchParams = new HttpParams();
+      searchParams = searchParams.append('print', 'pretty');
+      searchParams = searchParams.append('custom', 'key');      
+      return this.http      
+      .get(
+        'https://angular-httprequests-project.firebaseio.com/posts.json?print=pretty&custom=key',
+        {
+          headers: new HttpHeaders({'Custom-Header':'Hello'}),
+          responseType: 'json'
+        }
+      )
+      .pipe(
+        map(responseData => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        }),
+        catchError(errorRes => {
+          // Send to analytics server
+          return throwError(errorRes);
+        })
+      );    
+    }
+    deletePosts() {
+      return this.http.delete('https://angular-httprequests-project.firebaseio.com/posts.json',
+      {
+        observe: 'events',
+        responseType: 'text'
+      }
+      ).pipe(tap(event => {
+        console.log(event);
+        if (event.type === HttpEventType.Sent){
+          // ...
+        }
+        if (event.type === HttpEventType.Response) {
+          console.log(event.body);
+        }
+      })
+      );
+    }
+}
+
---
+++ 24_bonus_working_with_ngrx/my-first-app/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  firebaseAPIKey: 'AIzaSyC-_Xy8tGotQML5m36a8JhIOAbwjC91Q7I'
+};
